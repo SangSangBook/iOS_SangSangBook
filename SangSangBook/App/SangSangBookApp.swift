a008255fa26d7745a45f40206c4037a22010601c
ctokkm <2271488.hansung.ac.kr>
2025-03-11 19:02:36 +0900

    [init] 상상Book 프로젝트 Xcode 셋업

---
+++ SangSangBook/App/SangSangBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SangSangBookApp.swift
+//  SangSangBook
+//
+//  Created by KKM on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SangSangBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
