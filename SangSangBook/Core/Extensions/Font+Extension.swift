953dc11747473f7df32858bcc1dbe0a533bbfad5
ctokkm <2271488.hansung.ac.kr>
2025-03-13 23:38:59 +0900

    [feat] 폰트 추가 및 로그인 화면 적용

---
+++ SangSangBook/Core/Extensions/Font+Extension.swift
@@ -0,0 +1,33 @@
+//
+//  Font+Extension.swift
+//  SangSangBook
+//
+//  Created by KKM on 3/13/25.
+//
+
+import SwiftUI
+
+extension Font {
+    // **Jalnan2 폰트 스타일**
+    enum Jalnan2Style: String {
+        case regular = "Jalnan2"
+    }
+    
+    // **GyeonggiTitleOTF 폰트 스타일**
+    enum GyeonggiTitleOTFStyle: String {
+        case bold = "GyeonggiTitleOTF_Bold"
+        case light = "GyeonggiTitleOTF_Light"
+        case medium = "GyeonggiTitleOTF_Medium"
+        case regular = "GyeonggiTitleOTF_Regular"
+    }
+
+    // **Jalnan2 폰트 적용**
+    static func jalnan2(_ type: Jalnan2Style, size: CGFloat) -> Font {
+        return .custom(type.rawValue, size: size)
+    }
+
+    // **GyeonggiTitleOTF 폰트 적용** -> .font(.ggTitle(.bold, size: 22)) 와 같이 사용
+    static func ggTitle(_ type: GyeonggiTitleOTFStyle, size: CGFloat) -> Font {
+        return .custom(type.rawValue, size: size)
+    }
+}
--- SangSangBook/View/Login/LoginView.swift
+++ SangSangBook/View/Login/LoginView.swift
@@ -9,19 +9,21 @@ import SwiftUI
 
 struct LoginView: View {
     @StateObject private var viewModel = LoginViewModel()
+    @FocusState private var emailFieldIsFocused: Bool
+    @FocusState private var passwordFieldIsFocused: Bool
     
     var body: some View {
         NavigationView {
             ZStack {
                 Color(hex: "F2F3F7")
-                    .edgesIgnoringSafeArea(.all) // 안전 영역 무시하여 전체 적용
+                    .edgesIgnoringSafeArea(.all)
                 
                 VStack(spacing: 20) {
                     Spacer()
                     
                     Text("상상Book")
                         .padding(.bottom, 20)
-                        .font(.largeTitle)
+                        .font(.jalnan2(.regular, size: 40))
                         .fontWeight(.bold)
                         .foregroundStyle(Color(hex: "#C7CED9"))
                     
@@ -32,16 +34,22 @@ struct LoginView: View {
                             .frame(width: 21)
                         
                         TextField("아이디", text: $viewModel.email)
+                            .font(.ggTitle(.light, size: 20))
                             .keyboardType(.emailAddress)
                             .autocapitalization(.none)
+                            .focused($emailFieldIsFocused)
                             .padding(.leading, 20)
                     }
                     .padding(.horizontal, 20)
                     .padding(.vertical, 10)
                     .background(.white)
                     .cornerRadius(5)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(emailFieldIsFocused ? Color(hex: "0B4DA1") : Color.clear, lineWidth: 2)
+                    )
                     .padding(.horizontal, 60)
-                    
+
                     HStack {
                         Image("loginPw")
                             .resizable()
@@ -49,12 +57,18 @@ struct LoginView: View {
                             .frame(width: 17)
                         
                         SecureField("비밀번호", text: $viewModel.password)
+                            .font(.ggTitle(.light, size: 20))
+                            .focused($passwordFieldIsFocused)
                             .padding(.leading, 20)
                     }
                     .padding(.horizontal, 22)
                     .padding(.vertical, 10)
                     .background(.white)
                     .cornerRadius(5)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(passwordFieldIsFocused ? Color(hex: "0B4DA1") : Color.clear, lineWidth: 2)
+                    )
                     .padding(.horizontal, 60)
                     
                     if let errorMessage = viewModel.errorMessage {
@@ -63,13 +77,12 @@ struct LoginView: View {
                             .font(.caption)
                     }
                     
-                    // 로그인 성공 시 홈 화면으로 이동
                     NavigationLink(destination: HomeView(), isActive: $viewModel.isLoggedIn) {
                         Button(action: {
                             viewModel.login()
                         }) {
                             Text(viewModel.isLoading ? "로그인 중..." : "로그인")
-                                .font(.system(size: 30, weight: .bold))
+                                .font(.jalnan2(.regular, size: 30))
                                 .frame(maxWidth: .infinity)
                                 .padding(.vertical, 10)
                                 .background(Color(hex: "#0B4DA1"))
